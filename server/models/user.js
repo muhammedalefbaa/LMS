bd69afb9ea01e260c4dbe5e94cd0a78c5ee692fb
muhammed <mo.isknder@gmail.com>
2025-05-09 01:21:31 +0200

    edit user schema

--- server/models/user.js
+++ server/models/user.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
-    _id: { type: String, required: true },
+    id: { type: String, required: true },
     username: { type: String, required: true },
     email: { type: String, required: true },
     imageUrl: { type: String, required: true },
